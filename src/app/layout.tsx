06d47002e59cc036ef1aea8ce8cd72ed886e03f1
Osatohanmwen Solomon <solomonosato18@gmail.com>
2023-10-12 11:20:13 +0100

    Setup sonner(toasts)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,29 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import {  Poppins } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import { Toaster } from "sonner";
 
-const poppins = Poppins({ weight: ["400", "500", "600", "700"], subsets: ['latin'] })
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: 'Welcome | Sign-up',
-  description: 'A welcome page with a sign-up form',
-}
+  title: "Welcome | Sign-up",
+  description: "A welcome page with a sign-up form",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <Toaster richColors expand position="bottom-center" />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
