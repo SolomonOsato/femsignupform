9cfa374e7c79b12473a629a73b6d50bf638b7f12
Osatohanmwen Solomon <solomonosato18@gmail.com>
2023-10-12 11:29:37 +0100

    Fix padding on the promo card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Signup from "@/components/Signup";
 
 export default function Home() {
   return (
-    <main className="text-white font-normal bg-mobile bg-primary-red md:bg-desktop grid items-center gap-16 md:gap-4 md:grid-cols-2 py-16 px-6 md:px-32">
+    <main className="text-white font-normal bg-mobile bg-primary-red md:bg-desktop grid items-center gap-16 md:gap-4 md:grid-cols-2 py-16 px-6 md:px-24">
       <section className="grid gap-8 md:gap-4 text-center md:text-left">
         <h1 className="text-4xl font-bold">Learn to code by watching others</h1>
         <p className="text-white/90 text-xs">
